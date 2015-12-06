a5f50c0656627d8816f78b32e926fd4b68cdccc8
Johannes Schüth <git@jotschi.de>
2015-12-06 15:39:14 +0000

    Update pinout

--- dcb.py
+++ dcb.py
@@ -1,44 +1,8 @@
 #!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#  lcd_16x2.py
-#  16x2 LCD Test Script
-#
-# Author : Matt Hawkins
-# Date   : 06/04/2015
-#
-# http://www.raspberrypi-spy.co.uk/
-#
-#--------------------------------------
- 
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
- 
-#import
+
 import RPi.GPIO as GPIO
 import time
  
-# Define GPIO to LCD mapping
 LCD_RS = 25
 LCD_E  = 24
 LCD_D4 = 23
@@ -49,12 +13,12 @@ LED_TOGGLE = 22
 B1 = 18 
 B2 = 15
 B3 = 27
-B4 = 4
+#B4 = 4
 L1 = 8
 L2 = 17
 L3 = 28
 
-REED = 29
+REED = 4
 BAR = 2
  
 # Define some device constants
@@ -85,7 +49,7 @@ def main():
   GPIO.setup(B1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(B2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(B3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-  GPIO.setup(B4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#  GPIO.setup(B4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(REED, GPIO.IN)
   GPIO.setup(BAR, GPIO.OUT)
 #  GPIO.setup(B4, GPIO.IN)
@@ -95,7 +59,7 @@ def main():
   GPIO.add_event_detect(B1, GPIO.RISING, callback=b1callback, bouncetime=300)
   GPIO.add_event_detect(B2, GPIO.RISING, callback=b2callback, bouncetime=300)
   GPIO.add_event_detect(B3, GPIO.RISING, callback=b3callback, bouncetime=300)
-  GPIO.add_event_detect(B4, GPIO.RISING, callback=b4callback, bouncetime=300)
+#  GPIO.add_event_detect(B4, GPIO.RISING, callback=b4callback, bouncetime=300)
   GPIO.add_event_detect(REED, GPIO.FALLING, callback=reedCallback, bouncetime=300)
 
 
